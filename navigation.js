f22505218386f0f92f4a24b6ba2bcd28af870084
Yifan Sun <sunyifan112358@gmail.com>
2019-06-23 11:11:52 -0700

    Fix browser back

--- navigation.js
+++ navigation.js
@@ -1,54 +1,62 @@
-$(document).ready(() => {
-    const pages = [{
-            path: "/",
-            page: "/pages/home.html",
-        },
-        {
-            path: "/index.html",
-            page: "/pages/home.html",
-        },
-        {
-            path: "/publication.html",
-            page: "/pages/publication.html",
-            navBtn: 'nav-publication',
-        },
-        {
-            path: "/mgpusim.html",
-            page: "/pages/mgpusim.html",
-        }
-    ]
-
-    function route() {
-        let path = window.location.pathname
-        path = path.substr(1, path.length - 6)
-        if (path == "") {
-            path = "index"
-        }
-        const htmlPath = path + '.html'
-        const pagePath = "/pages/" + htmlPath
-        const navBtn = '#nav-' + path
-
-        loadContent(pagePath)
-        $('.nav-item').removeClass('active')
-        $(navBtn).addClass('active')
+const pages = [{
+        path: "/",
+        page: "/pages/home.html",
+    },
+    {
+        path: "/index.html",
+        page: "/pages/home.html",
+    },
+    {
+        path: "/publication.html",
+        page: "/pages/publication.html",
+        navBtn: 'nav-publication',
+    },
+    {
+        path: "/mgpusim.html",
+        page: "/pages/mgpusim.html",
     }
+]
+
+$(document).ready(() => {
     route();
 
     $('#nav-publication').click(() => {
-        window.history.pushState(null, null, "publication.html");
-        route()
+        jumpTo("publication.html")
     })
 
     $('#nav-index').click(() => {
-        window.history.pushState(null, null, "/");
-        route();
+        jumpTo("/")
     })
-
-    function loadContent(page) {
-        fetch(page)
-            .then(rsp => rsp.text())
-            .then(rsp => {
-                $('#right-content').html(rsp)
-            })
-    }
 })
+
+function jumpTo(pos) {
+    window.history.pushState(null, null, pos);
+    route();
+}
+
+function route() {
+    let path = window.location.pathname
+    path = path.substr(1, path.length - 6)
+    if (path == "") {
+        path = "index"
+    }
+    const htmlPath = path + '.html'
+    const pagePath = "/pages/" + htmlPath
+    const navBtn = '#nav-' + path
+
+    loadContent(pagePath)
+    $('.nav-item').removeClass('active')
+    $(navBtn).addClass('active')
+}
+
+function loadContent(page) {
+    fetch(page)
+        .then(rsp => rsp.text())
+        .then(rsp => {
+            $('#right-content').html(rsp)
+        })
+}
+
+window.addEventListener('popstate', function (event) {
+    route();
+}, false);
